41fac33cf220026d263003a0ff98c7fb44b09049
18wsk <william.kennedy@carbongraph.io>
2022-07-20 18:59:14 -0400

    first commit

---
+++ projectgallery/src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+export const Counter = () => {
+    const [counter, setCounter] = useState(0);
+    const urMom = 21;
+
+    const incrementCounter = () => {
+        setCounter(counter+1)
+    }
+
+    useEffect(()=> {
+        console.log(counter)
+    }, [counter, urMom])
+
+    return (
+        <div>
+            <p>{counter}</p>
+            <button onClick={incrementCounter}>MyButton</button>
+        </div>
+    )
+}
