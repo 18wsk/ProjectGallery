de24e6c133ece057f25fba45c7dfe4e1aa3d9222
18wsk <william.kennedy@carbongraph.io>
2022-08-04 08:29:21 -0400

    new branch for wills development

--- projectgallery/src/pages/will_profile.jsx
+++ projectgallery/src/pages/will_profile.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ParticlesBg from 'particles-bg'
+import MyImage from '../assets/will_pfp.jpg';
 
 function Willprofile() {
     return (
-        <div>will_profile</div>
+        <>
+            <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
+            <div className="flex items-center justify-center h-screen">
+                <img src={MyImage}  alt="..." width="200" height="200" className="rounded"/>
+                <div>hello</div>
+            </div>
+        </>
     )
 }
 
