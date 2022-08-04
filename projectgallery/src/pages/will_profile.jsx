9dce1c15f047516ce26b608fd27fec00c30a2684
willkennedy2000 <william.kennedy@carbongraph.io>
2022-08-04 08:58:14 -0400

    trying to init tailwind - not successful yet

--- projectgallery/src/pages/will_profile.jsx
+++ projectgallery/src/pages/will_profile.jsx
@@ -5,11 +5,14 @@ import MyImage from '../assets/will_pfp.jpg';
 function Willprofile() {
     return (
         <>
-            <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
+        <div className="w-full h-screen">
             <div className="flex items-center justify-center h-screen">
-                <img src={MyImage}  alt="..." width="200" height="200" className="rounded"/>
-                <div>hello</div>
+                <div className="border-4 border-slate-800">
+                    <img src={MyImage}  alt="..." width="200" height="200" className="rounded"/>
+                </div>
             </div>
+        </div>
+        <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
         </>
     )
 }
