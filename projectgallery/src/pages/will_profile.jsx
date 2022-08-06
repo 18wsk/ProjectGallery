58ddc7ef5def4f94939e3ccc6379fb487a38221b
18wsk <william.kennedy@carbongraph.io>
2022-08-06 11:48:51 -0400

    profile and about section started

--- projectgallery/src/pages/will_profile.jsx
+++ projectgallery/src/pages/will_profile.jsx
@@ -2,22 +2,49 @@ import React from 'react'
 import ParticlesBg from 'particles-bg'
 import MyImage from '../assets/will_pfp.jpg';
 import TopNavigation  from '../components/topNavigation'
+import '../styles/will_styles.css'
 
 function Willprofile() {
     return (
         <>
-        <head>
-            <link rel={'stylesheet'} href={'index.css'}/>
-        </head>
-        <div className="w-full h-screen">
-            <TopNavigation/>
-            <div className="flex items-center justify-center h-screen">
-                    <div >
-                        <img className="rounded-full border-4" src={MyImage}  alt="..." width="200" height="200" />
+        <TopNavigation/>
+        <div className="background">
+            <div className="griddy">
+                    <div className='selection'>
+                        <img className="circleprofile" src={MyImage}  alt="..." width="300" height="300" />
+                        <div className="section">
+                            <h1 className="name">William Kennedy</h1>
+                        </div>
+                    </div>
+                    <div className='text-section'>
+                        <div className="text-griddy">
+                            <div>
+                                <p>About me</p>
+                            </div>
+                            <div className="ineedspace">
+                                <p className="ppad">I am an energetic and passionate Computer Engineering student who is in their fifth year of computer engineering at Queen's University and is eagerly looking 
+                                    to apply my knowledge from both school and independent learning in a professional setting.
+                                </p>
+                            </div>
+                            <div>
+                                <p>Hobbies:</p>
+                            </div>
+                            <div className="ineedspace">
+                                <p className=""> Sports - specifically basketball, football and rugby </p>
+                                <p className=""> Competitive Esports - 4th in UPlay Canada tournament 2020, </p>
+                                <p className=""> Fitness and Board games.</p>
+                            </div>
+                            <div>
+                                <p>Socials:</p>
+                            </div>
+                            <div className="ineedspace">
+                            </div>
+                        </div>
+                        
                     </div>
             </div>
         </div>
-            <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
+        <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
         </>
     )
 }
