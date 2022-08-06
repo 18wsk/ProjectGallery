31f9206dd41fd37e43a550e7c3bae83bcc1c0991
18wsk <william.kennedy@carbongraph.io>
2022-08-06 09:43:51 -0400

    removing tailwind

--- projectgallery/src/App.jsx
+++ projectgallery/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Counter from './components/counter.jsx'
 import Homepage from './pages/homepage.jsx'
 import Willprofile from './pages/will_profile.jsx'
 
@@ -11,8 +10,6 @@ function App() {
                 <Route path="/" element={<Homepage/>}>
                 </Route>
                 <Route path="/willprofile" element={<Willprofile/>}>
-                </Route>
-                <Route path="/counter" element={<Counter/>}>
                 </Route>
             </Routes>
         </BrowserRouter>
--- projectgallery/src/pages/will_profile.jsx
+++ projectgallery/src/pages/will_profile.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ParticlesBg from 'particles-bg'
 import MyImage from '../assets/will_pfp.jpg';
+import TopNavigation  from '../components/topNavigation'
 
 function Willprofile() {
     return (
         <>
+        <head>
+            <link rel={'stylesheet'} href={'index.css'}/>
+        </head>
         <div className="w-full h-screen">
+            <TopNavigation/>
             <div className="flex items-center justify-center h-screen">
-                <div className="border-4 border-slate-800">
-                    <img src={MyImage}  alt="..." width="200" height="200" className="rounded"/>
-                </div>
+                    <div >
+                        <img className="rounded-full border-4" src={MyImage}  alt="..." width="200" height="200" />
+                    </div>
             </div>
         </div>
-        <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
+            <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
         </>
     )
 }
-
 export default Willprofile
