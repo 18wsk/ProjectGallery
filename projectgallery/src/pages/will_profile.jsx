7b9d3881d49b73ff8482a02bf22e611b81066532
willkennedy2000 <william.kennedy@carbongraph.io>
2022-08-07 17:07:22 -0400

    changed profile bg and layout

--- projectgallery/src/pages/will_profile.jsx
+++ projectgallery/src/pages/will_profile.jsx
@@ -1,22 +1,40 @@
 import React from 'react'
-import ParticlesBg from 'particles-bg'
 import MyImage from '../assets/will_pfp.jpg';
 import TopNavigation  from '../components/topNavigation'
 import '../styles/will_styles.css'
+import BackgroundParticles from '../components/backgroundParticles';
+import { FaLinkedinIn } from 'react-icons/fa'
+import { AiFillGithub } from 'react-icons/ai'
 
 function Willprofile() {
+    const handleLinkedenRedirect = () => {
+
+    }
     return (
         <>
         <TopNavigation/>
         <div className="background">
             <div className="griddy">
-                    <div className='selection'>
+                <div className='section'>
+                    <div className='section'>
                         <img className="circleprofile" src={MyImage}  alt="..." width="300" height="300" />
-                        <div className="section">
-                            <h1 className="name">William Kennedy</h1>
+                    </div>
+                    <div className="section">
+                        <h1 className="name">William Kennedy</h1>
+                        <h3 className="name">Engineering Student and Future Fullstack Developer</h3>
+                        <div className="socials-container">
+                            <div className="socials-grid">
+                                <div className='socials-container'>
+                                    <button className='linkeden' onClick={handleLinkedenRedirect}><a href="https://www.linkedin.com/in/william-kennedy-0a917319b/" target="_blank" rel="noreferrer"><FaLinkedinIn size="30px" color="white"/></a></button>
+                                </div>
+                                <div className='socials-container'>
+                                    <button className='github'><a href="https://github.com/18wsk" target="_blank" rel="noreferrer"><AiFillGithub size="30px" color="black"/></a></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className='text-section'>
+                </div>
+                    {/* <div className='text-section'>
                         <div className="text-griddy">
                             <div>
                                 <p>About me</p>
@@ -40,11 +58,10 @@ function Willprofile() {
                             <div className="ineedspace">
                             </div>
                         </div>
-                        
-                    </div>
+                    </div> */}
             </div>
         </div>
-        <ParticlesBg color="#c603fc" num={300} type="cobweb" bg={true} />
+        <BackgroundParticles/>
         </>
     )
 }
