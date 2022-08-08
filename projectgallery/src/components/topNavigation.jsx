d26660b10d5d05a3ff11af59573c57e914dac516
18wsk <william.kennedy@carbongraph.io>
2022-08-07 23:05:14 -0400

    new profile layout - no map

--- projectgallery/src/components/topNavigation.jsx
+++ projectgallery/src/components/topNavigation.jsx
@@ -4,6 +4,22 @@ import '../styles/navbar_styles.css'
 export default function TopNavigation() {
   return (
     <div className='navbar'>
+      <div>
+        <p>Home</p>
+      </div>
+      <div>
+        <p>About</p>
+      </div>
+      <div>
+        <p>Skills</p>
+      </div>
+      <div>
+        <p>Work</p>
+      </div>
+      <div>
+        <p>Contact</p>
+      </div>
+        
     </div>
   )
 }
--- projectgallery/src/pages/homepage.jsx
+++ projectgallery/src/pages/homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>hompage</div>
+    <div>
+    </div>
   )
 }
